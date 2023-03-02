6c2da01240dd374cd580ed5b4c0f43a9f7d74454
jiyuhang <syjzjyh@163.com>
2023-03-02 17:37:44 +0800

    feat: learn reactive

---
+++ src/views/learn/reactive/src/effect.ts
@@ -0,0 +1,37 @@
+// 依赖发生变化的时候执行fn函数 实现依赖收集和依赖的更新
+let activeEffect;
+export const effect = (fn: Function) => {
+  const _effect = function () {
+    activeEffect = _effect;
+    fn();
+  };
+
+  _effect();
+};
+
+const targetMap = new WeakMap();
+export const track = (target, key) => {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  let deps = depsMap.get(key);
+
+  if (!deps) {
+    deps = new Set();
+    depsMap.set(key, deps);
+  }
+
+  deps.add(activeEffect);
+};
+
+export const trigger = (target, key) => {
+  const depsMap = targetMap.get(target);
+  const deps = depsMap.get(key);
+
+  deps.forEach((effect) => {
+    effect();
+  });
+};
---
+++ src/views/learn/reactive/src/reactive.ts
@@ -0,0 +1,22 @@
+import { track, trigger } from "./effect";
+
+const isObject = (val: any): val is object =>
+  toString.call(val) === "[object Object]";
+export const reactive = <T extends object>(target: T) => {
+  return new Proxy(target, {
+    get(target, key, receiver) {
+      const res = Reflect.get(target, key, receiver);
+      track(target, key);
+
+      if (isObject(res)) {
+        return reactive(res);
+      }
+      return res;
+    },
+    set(target, key, value, receiver) {
+      const res = Reflect.set(target, key, value, receiver);
+      trigger(target, key);
+      return res;
+    },
+  });
+};
