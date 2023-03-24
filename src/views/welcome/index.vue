82dd95eb11578446036e87ed00bc0765d292e50d
masterjiyuhang <syjzjyh@163.com>
2023-03-25 00:30:01 +0800

    fix: update hooks

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -8,7 +8,24 @@
 <script setup lang="ts">
 import { useCounterStore } from "@/stores/modules/count";
 import { storeToRefs } from "pinia";
+import { useScript } from "@/hooks/useScript";
+import { onMounted } from "vue";
 
+const BAI_DU_MAP_URL =
+  "https://api.map.baidu.com/getscript?v=3.0&ak=OaBvYmKX3pjF7YFUFeeBCeGdy9Zp7xB2&services=&t=20210201100830&s=1";
+
+const { toPromise } = useScript({ src: BAI_DU_MAP_URL ?? "basic-api/system/getAccountList" });
+const init = async () => {
+  try {
+    const res = await toPromise();
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+};
+onMounted(async () => {
+  init();
+});
 const { increment } = useCounterStore();
 const { count } = storeToRefs(useCounterStore());
 </script>
