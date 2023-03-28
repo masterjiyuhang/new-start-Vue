49664638f98a0d9dc4f26ee9db9649ab3615610f
erHangdaDevil <jiyuhang@jctrans.net>
2023-03-28 18:41:39 +0800

    fix: update home page

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,30 +1,11 @@
 <template>
-  <div class="base-card home-page">
-    <img :src="welcome" class="home-img"/>
-    <!-- home page
-
-    {{ name }}
-
-    <el-input v-model="name"> </el-input>
-    {{ count }}
-    <el-button @click="increment">count++</el-button> -->
+  <div class="home-page">
+    <img :src="welcome" class="home-img" />
   </div>
 </template>
 
 <script setup lang="ts">
 import welcome from "@/assets/system/HalloweenIllustrations2.png";
-// import { useCounterStore } from "@/stores/modules/count";
-// import { useTestStore } from "@/stores/modules/test";
-
-// import { storeToRefs } from "pinia";
-// import { toRefs } from "vue";
-
-// const { increment } = useCounterStore();
-
-// const store = useTestStore();
-
-// const { name } = toRefs(store);
-// const { count } = storeToRefs(useCounterStore());
 </script>
 
 <style lang="scss" scoped>
@@ -32,13 +13,11 @@ import welcome from "@/assets/system/HalloweenIllustrations2.png";
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
-  height: 100%;
 
   .home-img {
-		width: 70%;
-		max-width: 800px;
-		margin-bottom: 20px;
-	}
+    object-fit: contain;
+    max-width: 100%;
+    max-height: 100%;
+  }
 }
 </style>
