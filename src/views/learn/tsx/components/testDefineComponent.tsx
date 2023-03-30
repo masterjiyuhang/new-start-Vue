08ec6d3403113bc5e241d1b3ef93404d2d21f70c
erHangdaDevil <jiyuhang@jctrans.net>
2023-03-30 23:30:01 +0800

    fix: udpate tsx

--- src/views/learn/tsx/components/testDefineComponent.tsx
+++ src/views/learn/tsx/components/testDefineComponent.tsx
@@ -1,7 +1,70 @@
-import { defineComponent } from "vue";
+/*
+ * @Author: erHangdaDevil
+ * @Date: 2023-03-30 09:08:20
+ * @LastEditors: erHangdaDevil jiyuhang@jctrans.net
+ * @LastEditTime: 2023-03-30 23:28:02
+ */
+
+import { defineComponent, ref } from "vue";
+
+/**
+ * ref template 自动解包.value tsx并不会自动解包
+ * v-show 支持
+ * v-if 不支持
+ * v-bind {}替代
+ * props
+ * emit
+ * slot
+ */
+
+interface Props {
+  name?: string;
+}
+
+const A = (_, { slots }) => (
+  <div>
+    <div>{slots.default ? slots.default() : "默认值"}</div>
+    <div>{slots.title?.()}</div>
+  </div>
+);
 
 export default defineComponent({
-  setup() {
-    return () => <div>testDefineComponent</div>;
+  props: {
+    name: String,
+  },
+  emits: ["onClick"],
+  setup(props: Props, { emit }) {
+    const data = [{ name: "cch" }, { name: "erhang" }];
+
+    const handleClick = (e: any) => {
+      console.log("点击了", e);
+      emit("onClick", e);
+    };
+
+    const pig = ref<string>("pig");
+    return () => (
+      <div>
+        testDefineComponent:{props.name}
+        <hr />
+        <input type="text" v-model={pig.value} />
+        <h2>{pig.value}</h2>
+        <hr />
+        <A
+          v-slots={{
+            default: () => <div> 插槽 默认 default slots</div>,
+            title: () => <div>插槽 默认 title</div>,
+          }}
+        />
+        <ul>
+          {data.map((item) => {
+            return (
+              <li onClick={() => handleClick(item)} name={item.name}>
+                {item.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   },
 });
--- src/views/learn/tsx/lesson1.vue
+++ src/views/learn/tsx/lesson1.vue
@@ -6,13 +6,17 @@
 
     <tsxComp2 :message="'父组件传递给tsx组件的信息'" />
 
-    <cch />
+    <cch :name="'传递进去的name'" @onClick="fooClick" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { tsxComp1, tsxComp2 } from "./components/tsxComp";
 import cch from "./components/testDefineComponent";
+
+const fooClick = (e) => {
+  console.log("父组件收到了", e);
+};
 </script>
 
 <style scoped></style>
