f4879a35f791300927ea2d308bcca58ccdeb11ac
erHangdaDevil <jiyuhang@jctrans.net>
2023-04-07 17:04:00 +0800

    fix: update car list

--- src/views/car/index.vue
+++ src/views/car/index.vue
@@ -7,10 +7,10 @@
 
     <el-table :data="tableData" style="width: 100%">
       <el-table-column fixed prop="date" label="Date" width="150" />
-      <el-table-column prop="name" label="Name" width="120" />
-      <el-table-column prop="state" label="State" width="120" />
-      <el-table-column prop="city" label="City" width="120" />
-      <el-table-column prop="address" label="Address" width="600" />
+      <el-table-column prop="name" label="Name" width="220" />
+      <el-table-column prop="state" label="State" width="220" />
+      <el-table-column prop="city" label="City" width="220" />
+      <el-table-column prop="address" label="Address" width="300" />
       <el-table-column prop="zip" label="Zip" width="120" />
       <el-table-column fixed="right" label="Operations" width="120">
         <template #default="scope">
