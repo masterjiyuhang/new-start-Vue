1a8db2785a995d4d63830b3e315d0a92f5af8c1a
erhang <syjzjyh@163.com>
2023-05-31 20:30:35 +0800

    fix: axios 不能区分环境导致的 axios.defaults.adapter 报错

--- src/api/carManagement.ts
+++ src/api/carManagement.ts
@@ -1,16 +1,27 @@
 import Req from "@/utils/request";
-import axios from "axios";
-import { cacheAdapterEnhancer, throttleAdapterEnhancer } from "axios-extensions";
+import {
+  cacheAdapterEnhancer,
+  throttleAdapterEnhancer,
+} from "axios-extensions";
 import adapter from "axios/lib/adapters/xhr";
 import adapter1 from "axios/lib/adapters/http";
+import axios from "axios";
+
+if (typeof XMLHttpRequest !== "undefined") {
+  // For browsers use XHR adapter
+  axios.defaults.adapter = adapter;
+} else if (typeof process !== "undefined") {
+  // For node use HTTP adapter
+  axios.defaults.adapter = adapter1;
+}
+
 const http = new Req({
   requestConfig: {
     isLoading: true,
   },
   axiosConfig: {
-    adapter: throttleAdapterEnhancer(
-      cacheAdapterEnhancer(adapter)
-    ),
+    adapter: throttleAdapterEnhancer(cacheAdapterEnhancer(adapter)),
+    // adapter: throttleAdapterEnhancer(cacheAdapterEnhancer(axios.defaults.adapter)),
     // adapter: throttleAdapterEnhancer(adapter1, {
     //   threshold: 2 * 1000,
     // }),
