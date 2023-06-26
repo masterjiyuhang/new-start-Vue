e344ebae556558a083d383f7a9a22722a30ac885
erhang <syjzjyh@163.com>
2023-06-26 09:54:21 +0800

    fix: update

--- src/views/car/BgFusion/index.vue
+++ src/views/car/BgFusion/index.vue
@@ -1,18 +1,23 @@
 <template>
   <wrapper-page>
-    <div class="fusion-container">asd</div>
+    <div class="fusion-container"></div>
 
     <TankShaking />
 
     <MobiusBand />
 
     <LoadingEffect />
+
+    <div class="reflect-box-container">
+      <div class="btn1">来吧 展示</div>
+      <div class="my-btn"></div>
+    </div>
   </wrapper-page>
 </template>
 
 <script setup lang="ts">
 import { WrapperPage } from "@/components/WrapperPage";
-import { TankShaking } from "./TankShaking";
+import { TankShaking } from "../TankShaking/TankShaking";
 import { MobiusBand } from "../MobiusBand/index";
 import { LoadingEffect } from "../LoadingEffect/index";
 </script>
@@ -67,6 +72,92 @@ import { LoadingEffect } from "../LoadingEffect/index";
   @keyframes moveToLeft {
     50% {
       margin-right: 0px;
+    }
+  }
+}
+
+.reflect-box-container {
+  display: flex;
+  flex: 1;
+  height: 500px;
+  justify-content: center;
+  background-color: bisque;
+  position: relative;
+
+  $color: #0ebeff;
+
+  @mixin btn {
+    z-index: 0;
+    width: 160px;
+    height: 80px;
+    line-height: 80px;
+    color: $color;
+    font-size: 24px;
+    border-radius: 10px;
+    text-align: center;
+    margin: auto;
+    transform: translateY(-40px);
+    overflow: hidden;
+    cursor: pointer;
+    transition: 0.3s;
+    position: relative;
+    -webkit-box-reflect: below 10px
+      linear-gradient(transparent, rgba(0, 0, 0, 0.4));
+
+    &:hover {
+      color: #fff;
+      box-shadow: 0 0 5px $color, 0 0 25px $color;
+
+      &::after,
+      &::before {
+        transition: 0.3s;
+        background-color: $color;
+      }
+    }
+
+    &::before {
+      content: "";
+      box-sizing: border-box;
+      position: absolute;
+      z-index: -2;
+      left: 50%;
+      top: 50%;
+      width: 150%;
+      height: 300%;
+      transform: translate(-50%, -50%);
+      background-color: #000;
+      background-repeat: no-repeat;
+      background-size: 50% 50%;
+      background-position: 0 0;
+      background-image: conic-gradient($color, $color);
+      animation: rotate 2s linear infinite;
+    }
+  }
+
+  .btn1 {
+    @include btn;
+    &::after {
+      content: "";
+      box-sizing: border-box;
+      position: absolute;
+      z-index: -1;
+      left: 2px;
+      top: 2px;
+      width: calc(100% - 4px);
+      height: calc(100% - 4px);
+      background-color: #000;
+      border-radius: 10px;
+    }
+  }
+
+  .btn2 {
+    @include btn;
+    filter: hue-rotate(90deg);
+  }
+
+  @keyframes rotate {
+    100% {
+      transform: translate(-50%, -50%) rotate(1turn);
     }
   }
 }
