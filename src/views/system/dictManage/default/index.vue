253dd7a495a7d46aab78877f5f9ea0268544703a
纪宇航 <jiyuhang@jctrans.net>
2024-01-11 21:33:44 +0800

    styles: update dict manage styles

--- src/views/system/dictManage/default/index.vue
+++ src/views/system/dictManage/default/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="wrapper-page">默认字典管理</div>
+  <div class="wrapper-page">
+    默认字典管理
+    <div :class="styles.container"></div>
+
+    <div :class="styles['base-container']"></div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import styles from "./index.module.scss";
+</script>
 
 <style scoped></style>
