0d4e5a993ef06b671fd3a707920a9a1b20f0c9c4
纪宇航 <jiyuhang@jctrans.net>
2024-03-01 13:19:06 +0800

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig(({ mode, command }: ConfigEnv): UserConfig => {
   const optimizeDeps = {};
 
   return {
-    base: "/",
+    base: "/new-start-Vue/",
 
     define: {
       "process.env": {},
