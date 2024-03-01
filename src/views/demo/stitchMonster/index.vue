520a17bc3913669ea3865409937a217905c4e1fe
纪宇航 <jiyuhang@jctrans.net>
2024-03-01 11:21:07 +0800

    chore: build config update

--- src/views/demo/stitchMonster/index.vue
+++ src/views/demo/stitchMonster/index.vue
@@ -7,9 +7,10 @@
 </template>
 
 <script setup lang="ts">
-import { HoneyComb } from "./HoneyComb/honeyComb";
+import { HoneyComb } from "./HoneyComb/HoneyComb";
 import { Stitch } from "./Stitch/Stitch";
 import TimeClock from "./TimeClock/index.vue";
 </script>
 
 <style scoped></style>
+./Stitch/Stitch
