f99c518e0bb2ce33319d48b89a7547ff1cf1a207
纪宇航 <jiyuhang@jctrans.net>
2024-03-01 13:04:31 +0800

    feat: 更新stitch组件

--- src/views/demo/stitchMonster/index.vue
+++ src/views/demo/stitchMonster/index.vue
@@ -2,13 +2,13 @@
   <div class="wrapper-page">
     <TimeClock />
     <HoneyComb />
-    <Stitch />
+    <!-- <Stitch /> -->
   </div>
 </template>
 
 <script setup lang="ts">
 import { HoneyComb } from "./HoneyComb/HoneyComb";
-import { Stitch } from "./Stitch/Stitch";
+// import { Stitch } from "./Stitch/Stitch";
 import TimeClock from "./TimeClock/index.vue";
 </script>
 
