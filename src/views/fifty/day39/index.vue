47d37a370671c3ea82b95937fb3b6898c72ee67e
纪宇航 <jiyuhang@jctrans.net>
2024-06-29 21:42:16 +0800

    feat: 初始化day39

---
+++ src/views/fifty/day39/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="background"></div>
+    <div class="p-10 text-center bg-white rounded shadow-md">
+      <h1 class="text-3xl">Image Password Strength</h1>
+      <p class="text-sm text-gray-700">Change the password to see the effect</p>
+      <div class="my-4 text-left">
+        <label for="email" class="text-gray-900">Email:</label>
+        <input
+          type="text"
+          class="block w-full p-2 mt-2 border rounded"
+          id="email"
+          placeholder="Enter Email"
+        />
+      </div>
+      <div class="my-4 text-left">
+        <label for="email" class="text-gray-900">Password:</label>
+        <input
+          type="password"
+          class="block w-full p-2 mt-2 border rounded"
+          id="password"
+          placeholder="Enter Password"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
