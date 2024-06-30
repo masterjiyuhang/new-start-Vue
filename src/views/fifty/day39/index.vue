18306ff3883312ee341ba4666f3571f180b6d230
纪宇航 <jiyuhang@jctrans.net>
2024-06-30 16:53:56 +0800

    fix: 更新day39

--- src/views/fifty/day39/index.vue
+++ src/views/fifty/day39/index.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <div class="background"></div>
-    <div class="p-10 text-center bg-white rounded shadow-md">
-      <h1 class="text-3xl">Image Password Strength</h1>
+  <div
+    class="relative flex flex-col items-center justify-center h-full m-0 overflow-hidden wrapper-page"
+  >
+    <div
+      class="background"
+      ref="bgRef"
+      :style="{
+        filter: `blur(${blur}px)`,
+      }"
+    ></div>
+    <div class="z-10 p-10 text-center bg-white rounded shadow-md">
+      <h1 class="text-3xl text-black">Image Password Strength</h1>
       <p class="text-sm text-gray-700">Change the password to see the effect</p>
       <div class="my-4 text-left">
         <label for="email" class="text-gray-900">Email:</label>
@@ -15,17 +23,39 @@
       </div>
       <div class="my-4 text-left">
         <label for="email" class="text-gray-900">Password:</label>
-        <input
+        <el-input
+          v-model="password"
           type="password"
           class="block w-full p-2 mt-2 border rounded"
-          id="password"
           placeholder="Enter Password"
+          @input="handlePwdInput"
         />
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const bgRef = ref();
+const blur = ref(20);
+const password = ref("");
+function handlePwdInput(input: string) {
+  // if (e.target) {
+  //   const input = (e.target as HTMLInputElement).value;
+  const length = input.length;
+  const blurValue = 20 - length * 2;
+  blur.value = blurValue > 0 ? blurValue : 0;
+  // }
+}
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.background {
+  position: absolute;
+  z-index: 0;
+  inset: -20px;
+  background: url("@/assets/images/login_left4.png") no-repeat center
+    center/cover;
+  filter: blur(20px);
+}
+</style>
