f745b08abb5fc01967928d3bc72badb775bdc3d6
纪宇航 <jiyuhang@jctrans.net>
2024-12-18 11:10:46 +0800

    feat: update day 49

--- src/views/fifty/day49/index.vue
+++ src/views/fifty/day49/index.vue
@@ -12,6 +12,53 @@
       <div class="card-2">
         <button class="delete-button">␡</button>
       </div>
+      <div class="flex items-center justify-center card-3">
+        <input type="checkbox" />
+      </div>
+
+      <div class="card-4">
+        <label class="relative inline-flex items-center cursor-pointer">
+          <input class="sr-only peer" value="" type="checkbox" />
+          <div
+            class="peer ring-2 ring-gray-900 bg-gradient-to-r from-rose-400 to-red-900 rounded-full outline-none duration-300 after:duration-500 w-20 h-8 shadow-inner peer-checked:bg-gradient-to-r peer-checked:from-emerald-500 peer-checked:to-emerald-900 shadow-gray-900 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-900 after:content-[''] after:rounded-full after:absolute after:outline-none after:h-12 after:w-12 after:bg-gray-50 after:-top-2 after:-left-2 after:flex after:justify-center after:items-center after:border-4 after:border-gray-900 peer-checked:after:translate-x-14"
+          ></div>
+        </label>
+      </div>
+
+      <div class="flex items-center justify-center card-5">
+        <button class="btn">Now</button>
+      </div>
+
+      <div class="flex items-center justify-center card-6">
+        <button class="button">
+          <span
+            class="bg-[#f1f5f8] block px-4 py-4 rounded-md border-2 border-solid border-red-950"
+            >Submit</span
+          >
+        </button>
+      </div>
+
+      <div class="flex flex-col items-center justify-center p-10 card-7">
+        <button
+          class="relative py-2 px-8 text-black text-base font-bold overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
+        >
+          HOVER ME!
+        </button>
+        <div class="my-5"></div>
+        <button
+          :class="[
+            'px-6 py-3',
+            'rounded-full shadow-lg',
+            'font-bold text-white',
+            'transition-all duration-500 ease-in-out',
+            'transform bg-gradient-to-r from-purple-400 to-blue-500',
+            'hover:from-pink-500 hover:to-purple-600 hover:scale-110 hover:brightness-110 hover:animate-pulse',
+            'active:animate-bounce',
+          ]"
+        >
+          Keep holding
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -240,6 +287,116 @@
 
   .delete-button:focus {
     outline: none;
+  }
+}
+
+.card-3 {
+  input[type="checkbox"] {
+    position: relative;
+    width: 96px;
+    height: 48px;
+    transition: all 0.5s;
+    border: 3px solid #222;
+    border-radius: 30px 100px 100px;
+    outline: none;
+    background-color: #fff;
+    box-shadow: 0 10px 20px rgb(0 0 0 / 20%);
+    cursor: pointer;
+    appearance: none;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 0;
+      box-sizing: border-box;
+      width: 48px;
+      height: 48px;
+      transform: translateY(-50%) scale(0.7);
+      transition: all 0.5s;
+      border: 3px solid #222;
+      border-radius: 30px 100px 100px;
+      background-color: #fde881;
+    }
+
+    &:checked {
+      border-radius: 100px 100px 30px;
+      background-color: #fde881;
+
+      &::before {
+        left: 50%;
+        border-radius: 100px 100px 30px;
+        background-color: #fff;
+      }
+    }
+  }
+}
+
+.card-5 {
+  .btn {
+    position: relative;
+    padding: 1.1em 2em;
+    overflow: hidden;
+    transition: all 0.3s;
+    border: 2px solid #fff;
+    border-radius: 12px;
+    background: none;
+    background-color: #ecd448;
+    box-shadow: 0 2px 0 2px #000;
+    color: #131313;
+    font-size: 15px;
+    font-weight: bolder;
+    cursor: pointer;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 50%;
+      width: 100px;
+      height: 120%;
+      transform: skewX(30deg) translate(-150%, -50%);
+      transition: all 0.5s;
+      background-color: #ff6700;
+    }
+
+    &:hover {
+      background-color: #4cc9f0;
+      box-shadow: 0 2px 0 2px #0d3b66;
+      color: #fff;
+
+      &::before {
+        transform: skewX(30deg) translate(150%, -50%);
+        transition-delay: 0.1s;
+      }
+    }
+
+    &:active {
+      transform: scale(0.9);
+    }
+  }
+}
+
+.card-6 {
+  .button {
+    width: 150px;
+    padding: 0;
+    padding-bottom: 3px;
+    transform: rotate(5deg);
+    transform-origin: center;
+    border: none;
+    border-radius: 5px;
+    background-color: #f80;
+    box-shadow: 0 2px 0 #494a4b;
+    font-family: "Gochi Hand", cursive;
+    font-size: 15px;
+    text-decoration: none;
+    cursor: pointer;
+
+    &:active {
+      padding-bottom: 0;
+      transform: translateY(5px);
+      outline: 0;
+    }
   }
 }
 </style>
