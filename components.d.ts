3915f9caa389401d78c0f837d6a05b39dc9cac1e
纪宇航 <jiyuhang@jctrans.net>
2025-09-24 13:33:39 +0800

    chore: up deps

--- components.d.ts
+++ components.d.ts
@@ -15,6 +15,7 @@ declare module 'vue' {
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
+    ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCol: typeof import('element-plus/es')['ElCol']
     ElCollapse: typeof import('element-plus/es')['ElCollapse']
     ElCollapseItem: typeof import('element-plus/es')['ElCollapseItem']
